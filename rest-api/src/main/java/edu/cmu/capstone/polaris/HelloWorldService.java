6e479a72fb2f17d828af8f09b7c6260b70e05d8b
Tony Liu <tonyhualiu1912@gmail.com>
2014-10-05 23:01:11 -0400

    add swagger dependency and user interface

--- rest-api/src/main/java/edu/cmu/capstone/polaris/HelloWorldService.java
+++ rest-api/src/main/java/edu/cmu/capstone/polaris/HelloWorldService.java
@@ -4,13 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
  
 @Path("/hello")
+@Api(value = "/hello", description = "say hello to a name")
 public class HelloWorldService {
  
 	@GET
 	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
+	@ApiOperation(value="just to test the sample api")
+	public Response getMsg(@ApiParam(value="param",required=true)@PathParam("param") String msg) {
  
 		String output = "Jersey say : " + msg;
  
